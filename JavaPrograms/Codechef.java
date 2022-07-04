b39feb740f8bb1d107a6184f373ee574c9b5c81c
Jay <jayjani002@gmail.com>
2022-07-04 20:52:00 +0530

    images complete

--- JavaPrograms/Codechef.java
+++ JavaPrograms/Codechef.java
@@ -1,38 +1,75 @@
 package JavaPrograms;
 
-import java.util.Scanner;
+import java.util.*;
 
 class Codechef {
-    public static void main(String[] args) throws java.lang.Exception {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) throws java.lang.Exception {
+    Scanner sc = new Scanner(System.in);
 
-        int T = scanner.nextInt();
+    int T = sc.nextInt();
 
-        while (T-- > 0) {
-            int n = scanner.nextInt();
-            char arr[] = new char[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = scanner.next().charAt(0);
-            }
+    while (T-- > 0) {
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      int k = sc.nextInt();
 
-            for (int i = 0; i < n; i++) {
-                if (arr[i] == 'A') {
-                    arr[i] = 'T';
-                } else if (arr[i] == 'T') {
-                    arr[i] = 'A';
-                } else if (arr[i] == 'G') {
-                    arr[i] = 'C';
-                } else {
-                    arr[i] = 'G';
-                }
-            }
+      int A[] = new int[n];
+      for (int i = 0; i < n; i++) {
+        A[i] = sc.nextInt();
+      }
 
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i]);
-            }
-            System.out.println();
+      Arrays.sort(A);
+
+      HashMap<Integer, Integer> lessK = new HashMap<>();
+      HashMap<Integer, Integer> moreK = new HashMap<>();
+      HashMap<Integer, Integer> equalK = new HashMap<>();
+
+      for (int i = 0; i < n; i++) {
+        if (A[i] < k) {
+          lessK.put(i, A[i]);
         }
 
-        scanner.close();
+        else if (A[i] > k) {
+          moreK.put(i, A[i]);
+        }
+
+        else {
+          equalK.put(i, A[i]);
+        }
+      }
+
+      System.out.println("Less than K: " + lessK);
+      System.out.println("Equal to K: " + equalK);
+      System.out.println("More than K: " + moreK);
+
+      boolean allLessThanK = false;
+      for (int i = 0; i < k; i++) {
+        if (lessK.containsValue(i)) {
+          allLessThanK = true;
+        } else {
+          allLessThanK = false;
+          break;
+        }
+      }
+
+      System.out.println("All less than K: " + allLessThanK);
+
+      if (allLessThanK && (lessK.size() + moreK.size() >= m)) {
+        System.out.println("Yes\n");
+      } else {
+        System.out.println("No\n");
+      }
+
     }
+
+    sc.close();
+  }
+
+  static void printArray(int arr[]) {
+    System.out.println("\nArray: ");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
 }
--- models/campground.js
+++ models/campground.js
@@ -4,40 +4,47 @@ const Schema = mongoose.Schema;
 const Review = require('./review');
 
 // Structure of campground collection in db:
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
-    title: String,
-    images: [
-        {
-            url: String,
-            filename: String,
-        },
-    ],
-    price: Number,
-    description: String,
-    location: String,
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review',
-        },
-    ],
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
+  title: String,
+  images: [ImageSchema],
+  price: Number,
+  description: String,
+  location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
     },
+  ],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 // Deleting all reviews assosiated with a campground that is to be deleted
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if (doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews,
-            },
-        });
-    }
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 // mongoose.model('Campground', CampgroundSchema);
 // This creates a collection
 module.exports = mongoose.model('Campground', CampgroundSchema);
+
+// JayJ1927127
