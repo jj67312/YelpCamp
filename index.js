5415661e962c114f347dfff16cdd7d8a696a33b9
Jay <jayjani002@gmail.com>
2022-07-10 01:55:33 +0530

    add start script

--- index.js
+++ index.js
@@ -224,9 +224,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, (req, res) => {
-  console.log('YelpCamp running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, (req, res) => {
+  console.log(`YelpCamp running on port ${port}`);
 });
 
-// password for mongo Atlas:
-// VEwTrWVkJyCazgrP
